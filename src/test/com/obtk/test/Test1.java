74118d5836357b9fd3e67bdedef9a03a4967ed56
xx-Boundless <2785270265@qq.com>
2020-12-28 14:21:37 +0800

    Initial commit

---
+++ src/test/com/obtk/test/Test1.java
@@ -0,0 +1,20 @@
+package com.obtk.test;
+
+import org.junit.Test;
+
+/**
+ * @author Xiaobei
+ * @date 2020-12-24 09:10:40
+ */
+public class Test1 {
+    @Test
+    public void testLm(){
+        System.out.println("测试代码是否能执行--lm");
+    }
+
+    @Test
+    public void textjk(){
+        System.out.println("jjjjjj");
+        System.out.println("zxdzxdzxd");
+    }
+}
